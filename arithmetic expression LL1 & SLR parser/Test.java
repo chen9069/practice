646945fa23840c867769e809c50e85b14c1f0eee
chen9069 <cj815@nyu.edu>
2015-11-10 20:59:52 -0500

    Update Test.java

--- arithmetic expression LL1 & SLR parser/Test.java
+++ arithmetic expression LL1 & SLR parser/Test.java
@@ -24,17 +24,7 @@ public class Test {
 			Expression expr = parser.parse();
 			expr.accept(Printer.getInstance());
 			System.out.println("=" + expr.accept(Evaluator.getInstance()));
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (TokenizeException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (ParseException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		try {			
+			
 			System.out.println("SLR:");
 			InputStream in = new ByteArrayInputStream(s.getBytes());
 			syntaxAnalysis.SLR.Parser parser = new syntaxAnalysis.SLR.Parser(in);
